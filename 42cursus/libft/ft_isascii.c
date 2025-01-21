cc1f70bdf18c882dd31b8e6b1373fa1bf60f051a
mregueir <marioregueiroliria@gmail.com>
2025-01-21 10:31:35 +0100

    ft_isascii

---
+++ 42cursus/libft/ft_isascii.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mregueir <mregueir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/21 10:24:58 by mregueir          #+#    #+#             */
+/*   Updated: 2025/01/21 10:30:32 by mregueir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_isascii(int c)
+{
+	if (c >= 0 && c <= 127)
+	{
+		return (c);
+	}
+	return (0);
+}
+
+// #include <stdio.h>
+
+// int main(void){
+//     int miau = ft_isascii(129);
+//     if(miau != 0)
+//         printf("Es US ASCII. Código ASCII = %d", miau);
+//     else   
+//         printf("No es US ASCII");
+//     return (0);
+// }
