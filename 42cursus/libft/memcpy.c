f138277850610a554a15f0b6e9d815541444a32c
miauro <marioregueiroliria@gmail.com>
2025-01-22 12:11:08 +0100

    memcpy no norminette

---
+++ 42cursus/libft/memcpy.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   memcpy.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mregueir <mregueir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/22 11:50:12 by mregueir          #+#    #+#             */
+/*   Updated: 2025/01/22 11:50:12 by mregueir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void *ft_memcpy(void *dest, const void *src, size_t num)
+{
+    size_t i;
+    unsigned char *origin;
+    unsigned char *destination;
+
+    origin = (unsigned char *)src;
+    destination = (unsigned char *)dest;
+    i = 0;
+    while (i < num)
+    {
+        destination[i] = origin[i];
+        i++;
+    }
+    return ((void *)destination);
+}
+
+// int main()
+// {
+//     const char src[50] = "Miauuuuuuuu";
+//     char dest[50] = "aaaaaaaaa";
+
+//     printf("Before ft_memcpy dest = %s\n", dest);
+//     ft_memcpy(dest, src, 4);
+//     printf("After ft_memcpy dest = %s\n", dest);
+//     return (0);
+// }
