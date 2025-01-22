aab903be245cd1bdac5a446c8269a233e3a232a9
miauro <marioregueiroliria@gmail.com>
2025-01-22 13:46:59 +0100

    strlcat strlcpy no norminette

---
+++ 42cursus/libft/ft_strlcat.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mregueir <mregueir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/22 13:20:16 by mregueir          #+#    #+#             */
+/*   Updated: 2025/01/22 13:20:16 by mregueir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+extern size_t ft_strlcat(char *dest, char *src, size_t size)
+{
+    size_t i;
+    size_t j;
+    size_t length;
+
+    j = 0;
+    i = 0;
+    length = 0;
+    while (src[j] != 0)
+        j++;
+    while (dest[i] != 0)
+        i++;
+    if (size <= i)
+        length = j + size;
+    else
+        length = i + j;
+    j = 0;
+    while (src[j] != 0 && (i + 1) < size)
+    {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+    dest[i] = 0;
+    return (length);
+}
+
+/*
+int main() {
+    char dest[50] = "Hola, ";
+    char src[] = "mundo!";
+    int resultado = ft_strlcat(dest, src, 230);
+    printf("Cadena: %s\n", dest);
+    printf("Longitud: %u\n", resultado);
+    return 0;
+}*/
---
+++ 42cursus/libft/ft_strlcpy.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mregueir <mregueir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/22 13:29:10 by mregueir          #+#    #+#             */
+/*   Updated: 2025/01/22 13:29:10 by mregueir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+extern size_t ft_strlcpy(char *dest, char *src, size_t size)
+{
+    size_t i;
+    size_t j;
+
+    j = 0;
+    i = 0;
+    while (src[j] != 0)
+        j++;
+    if (size > 0)
+    {
+        while (src[i] != 0 && i < (size - 1))
+        {
+            dest[i] = src[i];
+            i++;
+        }
+        dest[i] = 0;
+    }
+    return (j);
+}
+
+// int main()
+// {
+//     char dest[50] = "Hola, ";
+//     char src[] = "mundo!";
+//     int resultado = ft_strlcpy(dest, src, 230);
+//     printf("Cadena: %s\n", dest);
+//     printf("Longitud: %u\n", resultado);
+//     return 0;
+// }
