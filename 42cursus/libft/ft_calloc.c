c1862eb63761aa2a135004f2af7d7a445c1a1b16
miauro <marioregueiroliria@gmail.com>
2025-01-26 01:57:42 +0100

    calloc & strdup nocheck

---
+++ 42cursus/libft/ft_calloc.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mregueir <mregueir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/25 17:47:59 by mregueir          #+#    #+#             */
+/*   Updated: 2025/01/25 17:47:59 by mregueir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void *ft_calloc(size_t items, size_t size)
+{
+    void *ptr;
+    size_t i;
+
+    i = 0;
+    if (items == 0 || size == 0 || (items * size) / size != items)
+        return (0);
+    ptr = malloc(items * size);
+    if (ptr == 0)
+        return (0);
+    while (i < (items * size))
+    {
+        ((unsigned char *)ptr)[i] = 0;
+        i++;
+    }
+    return ((void *)ptr);
+}
---
+++ 42cursus/libft/ft_strdup.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mregueir <mregueir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/25 18:29:40 by mregueir          #+#    #+#             */
+/*   Updated: 2025/01/25 18:29:40 by mregueir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *ft_strdup(const char *str1)
+{
+    char *str2;
+    char *str1b;
+
+    str1b = (char *)str1;
+    int i;
+
+    i = 0;
+    while (str1b[i] != 0)
+        i++;
+    str2 = (char *)malloc(i * sizeof(char));
+    if (str2 == 0)
+        return (0);
+    i = 0;
+    while (str1b[i] != 0)
+    {
+        str2[i] = str1b[i];
+        i++;
+    }
+    str2[i] = 0;
+    return (str2);
+}
+
+// int main()
+// {
+//     char *str1 = "miau";
+//     printf("Como podemos observar, %s es igual a %s", str1, ft_strdup(str1));
+// }
