ca8afd3c40e59336faa4ad0e8a33594f67719ab8
mregueir <marioregueiroliria@gmail.com>
2025-01-27 15:21:17 +0100

    ft_strtrim

---
+++ 42cursus/libft/ft_strtrim.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mregueir <mregueir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/27 14:43:06 by mregueir          #+#    #+#             */
+/*   Updated: 2025/01/27 15:20:55 by mregueir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int	ft_isset(const char *set, char c)
+{
+	int	i;
+
+	i = 0;
+	while (set[i] != 0)
+	{
+		if (set[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	ft_size(char const *s1, const char *set)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (s1[j] != 0)
+		j++;
+	while (ft_isset(set, s1[i]) == 1)
+		i++;
+	j--;
+	while (ft_isset(set, s1[j]) == 1)
+		j--;
+	return (j - i);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	int		i;
+	char	*st;
+	int		size;
+	int		j;
+
+	size = ft_size(s1, set);
+	printf("%d\n", size);
+	i = 0;
+	j = 0;
+	st = (char *)malloc(size * sizeof(char));
+	if (st == NULL)
+		return (NULL);
+	while (ft_isset(set, s1[i]) == 1)
+		i++;
+	while (j < (size + 1))
+	{
+		st[j] = s1[i];
+		i++;
+		j++;
+	}
+	return (st);
+}
+
+// int	main(void)
+// {
+// 	char *st = ft_strtrim("-- --    -miau- - - --", "- ");
+// 	printf("%s", st);
+// 	return (0);
+// }
