bf253a84913a1f263c1c2fdaefe9661e9852f9b4
mregueir <marioregueiroliria@gmail.com>
2025-01-27 14:05:06 +0100

    ft_strjoin.c primera version

---
+++ 42cursus/libft/ft_strjoin.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mregueir <mregueir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/27 13:50:41 by mregueir          #+#    #+#             */
+/*   Updated: 2025/01/27 14:04:41 by mregueir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int	ft_size(char const *s1, char const *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] != 0)
+		i++;
+	while (s2[i] != 0)
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char *s3;
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	s3 = (char *)malloc(i * sizeof(char));
+	if (s3 == NULL)
+		return (NULL);
+	while (s1[i] != 0)
+	{
+		s3[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != 0)
+	{
+		s3[i] = s2[j];
+		i++;
+		j++;
+	}
+}
